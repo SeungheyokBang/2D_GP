7aed2f234b5f2c80e8e79f9c362352a4493102fe
SeungheyokBang <ac6042@nate.com>
2016-11-09 23:13:37 +0900

    dd
    dd

--- Metal_Slug1_0.1.0.0.1/Metal_Main.py
+++ Metal_Slug1_0.1.0.0.1/Metal_Main.py
@@ -1,236 +1,130 @@
-import random
-import json
-import os
-
-
 from pico2d import *
-from Stage_1 import *
+from Bcakground import *
+
+LEFT,RIGHT, UP = 0,1,2
+STAND, RUN, SHOT, JUMP, STAB, SWING,MOVEJUMP = 0,1,2,3,4,5,6
 
 
-import game_framework
-import title_state
-
-name = "MainState"
-
-class Marco:
-
-
+class Character_body():
     def __init__(self):
+        self.x, self.y, self.frame, self.shot, self.stab, self.swing = 100,150,0, 0,0,0
+        self.image = load_image('CHAR_body_S_R_S.png')
 
-
-
-        self.x, self.y =200, 150
-        self.move_frame = 0
-        self.jump_frame = 0
-        self.act_frame =0
-        self.shot_frame = 0
-
-        self.direction = 'right'
-
-        self.move_state = 'idle'
-        self.jump_state = 'ready'
-        self.act_state = 'ready'
-        self.shot_state = 'ready'
-
-        self.isjump = False
-
-        self.Move_Run = load_image('Move_Run.png')
-        self.Act_IdleLookUp=load_image('Act_IdleLookup.png')
-        self.Act_SitDown =load_image('Act_SitDown.png')
-        self.Move_Idle = load_image('Move_Stop.png')
-        self.Shot_IdleShot = load_image('Shot_idleShot.png')
-
-    def handle_event(self, event):
-        if event.type == SDL_KEYDOWN:
-
-            if event.key == SDLK_LEFT:
-                    self.move_state = 'run'
-                    self.direction = 'left'
-
-                    self.act_state = 'ready'
-                    self.shot_state = 'ready'
-                    self.move_frame = 0
-                    self.act_frame = 0
-                    self.shot_frame = 0
-
-            elif event.key == SDLK_RIGHT:
-                    self.move_state = 'run'
-                    self.direction = 'right'
-
-                    self.act_state = 'ready'
-                    self.shot_state = 'ready'
-                    self.move_frame = 0
-                    self.act_frame = 0
-                    self.shot_frame = 0
-            # Attack
-            if event.key == SDLK_a:
-                self.shot_state = 'shot'
-                self.shot_frame = 0
-
-            # Up
-            if event.key == SDLK_UP:
-                self.act_state = 'lookup'
-                self.act_frame = 0
-
-            # SitDown
-            if event.key == SDLK_DOWN:
-                self.act_state = 'sitdown'
-                self.act_frame = 0
-
-            # Jump
-            if event.key == SDLK_s:
-                if self.isjump == False:
-                    self.jump_state = 'jump_up'
-                    self.act_frame = 0
-                    self.isjump = True
-
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT and self.move_state == 'run':
-                self.move_state = 'idle'
-                self.move_frame = 0
-            elif event.key == SDLK_LEFT and self.move_state == 'run':
-                self.move_state = 'idle'
-                self.move_frame = 0
-            elif event.key == SDLK_DOWN:
-                self.move_state = 'idle'
-                self.act_state = 'ready'
-                self.act_frame = 0
-            elif event.key == SDLK_UP:
-                self.act_frame = 8
+    def move(self, key,direction,state):
+        self.direction = direction
+        self.state = state
+        if self.direction == RIGHT:
+            if self.state == STAB:
+                self.image = load_image('CHAR_body_stab.png')
+            elif self.state == SWING:
+                self.image = load_image('CHAR_body_swing.png')
+            elif self.state == JUMP:
+                self.image = load_image('CHAR_body_jump.png')
+            else:
+                self.image = load_image('CHAR_body_S_R_S.png')
+        elif self.direction == LEFT:
+            self.image = load_image('CHAR_body_S_R_S_re.png')
+        if key == RIGHT:
+            if self.x <400:
+                self.x += 10
+            self.frame = (self.frame + 1) % 10
+        elif key == LEFT:
+            if self.x > 55:
+                self.x -= 10
+            self.frame = (self.frame + 1) % 10
+        elif key == UP:
+            self.y += 5
+            self.frame = (self.frame + 1) % 10
+        if self.state == SHOT:
+            self.shot = (self.shot + 1)
+        if self.state == STAB:
+            self.stab = (self.stab + 1)
+        if self.state == SWING:
+            self.swing = (self.swing + 1)
 
     def update(self):
+        if self.state == STAND:
+            self.frame = (self.frame + 1 ) % 10
 
-            if(self.move_state in ('run','idle')):
-               self.move_frame = (self.move_frame + 1) % 11
+    def draw(self):
+        if self.state == STAND:
+            if self.direction == RIGHT:
+                self.image.clip_draw(56 * self.frame, 30*self.state, 56, 30, self.x, self.y,110,90)
+            elif self.direction == LEFT:
+                self.image.clip_draw(560-(56 * (self.frame+1)), 30*self.state, 56, 30, self.x-56, self.y,110,90)
+        elif self.state == RUN:
+            if self.direction == RIGHT:
+                self.image.clip_draw(56 * self.frame, 30*self.state, 56, 30, self.x, self.y,110,90)
+            elif self.direction == LEFT:
+                self.image.clip_draw(560-(56 * (self.frame+1)), 30*self.state, 56, 30, self.x-56, self.y,110,90)
+        elif self.state == SHOT:
+            if self.direction == RIGHT:
+                self.image.clip_draw(56 * self.shot, 30*self.state, 56, 30, self.x-10, self.y+3,110,90)
+            elif self.direction == LEFT:
+                self.image.clip_draw(560-(56 * (self.shot+1)), 30*self.state, 56, 30, self.x-56+10, self.y+3,110,90)
+        elif self.state == STAB:
+            if self.direction == RIGHT:
+                self.image.clip_draw(56 * self.stab, 0, 56, 35, self.x, self.y,110,90)
+            elif self.direction == LEFT:
+                self.image.clip_draw(560-(56 * (self.frame+1)), 0, 56, 30, self.x-56, self.y,110,90)
+        elif self.state == SWING:
+            if self.direction == RIGHT:
+                self.image.clip_draw(56 * self.swing, 0, 56, 50, self.x-25, self.y+10,120,120)
+            elif self.direction == LEFT:
+                self.image.clip_draw(560-(56 * (self.frame+1)), 0, 56, 30, self.x-56, self.y,110,90)
+        elif self.state == JUMP:
+            if self.direction == RIGHT:
+                self.image.clip_draw(56 * self.frame, 0, 56, 40, self.x, self.y,110,90)
+            elif self.direction == LEFT:
+                self.image.clip_draw(560-(56 * (self.frame+1)), 0, 56, 40, self.x-56, self.y,110,90)
 
+class Character_leg():
+    def __init__(self):
+        self.x, self.y, self.frame = 85, 105 ,0
+        self.image = load_image('CHAR_leg_S_R_J_M.png')
 
-            if(self.shot_state in ('shot')):
-                if self.shot_frame < 11:
-                      self.shot_frame = self.shot_frame + 1
-                else:
-                  self.shot_state='ready'
-                  self.move_frame = 0
-                  self.shot_frame = 0
+    def move(self, key,direction,state):
+        self.direction = direction
+        self.state = state
+        if self.direction == RIGHT:
+            self.image = load_image('CHAR_leg_S_R_J_M.png')
 
-
-            if(self.act_state in ('sitdown')):
-                if self.act_frame < 10:
-                    self.act_frame = self.act_frame + 1
-                else:
-                    self.act_frame = 4
-            elif(self.act_state in ('lookup')):
-                if self.act_frame < 7:
-                    self.act_frame = self.act_frame + 1
-                elif self.act_frame == 7:
-                    self.act_frame = 5
-                elif self.act_frame > 7 and self.act_frame < 11:
-                    self.act_frame = self.act_frame + 1
-                else:
-                    self.move_state = 'idle'
-                    self.act_state = 'ready'
-                    self.act_frame = 0
-
-
-            if self.move_state == 'run':
-                if self.act_state == 'ready':
-                    if self.direction == 'right':
-                         self.x = self.x + 5
-                    elif self.direction == 'left':
-                         self.x = self.x - 5
-                print('x pos %d' % self.x)
-
-            if self.jump_state == 'jump_up' and self.isjump == True:
-                self.jump_frame= (self.jump_frame + 1)
-                if self.jump_frame == 6:
-                    self.jump_state == 'jump_down'
-                else:
-                    self.y = self.y + 5
-                print('up jump frame %d' % self.jump_frame)
-
-
-            if self.jump_state == 'jump_down' and self.isjump == True:
-                self.jump_frame= (self.jump_frame - 1)
-                if self.jump_frame == 0:
-                    self.jump_state == 'ready'
-                    self.isjump = False
-                else:
-                    self.y = self.y - 5
-                print('down jump frame %d' % self.jump_frame)
-
+        elif self.direction == LEFT:
+            self.image = load_image('CHAR_leg_S_R_J_M_re.png')
+        if key == RIGHT:
+            if self.x + 15 <400:
+                self.x +=10
+            self.frame = (self.frame + 1 ) % 10;
+        if key == LEFT:
+            if self.x + 15 > 55:
+                self.x -= 10
+            self.frame = (self.frame + 1) % 10;
+        elif key == UP:
+            self.y+=5
+            self.frame = (self.frame +1) % 10
+    def update(self):
+        pass
+#        if self.state == STAND:
+#            self.frame = (self.frame + 1 ) % 10
 
 
     def draw(self):
-        if self.move_state == 'idle':
-            if self.shot_state == 'shot':
-                if self.direction == 'right':
-                    self.Shot_IdleShot.clip_draw(self.shot_frame * 142,  150, 142, 150, self.x, self.y)
-                elif self.direction == 'left':
-                    self.Shot_IdleShot.clip_draw((142*11) - ((self.shot_frame+1) * 142),  0, 142, 150, self.x, self.y)
-            elif self.shot_state == 'ready':
-                if self.act_state == 'ready':
-                    if self.direction == 'right':
-                        self.Move_Idle.clip_draw(self.move_frame * 125,  150, 125, 150, self.x, self.y)
-                    elif self.direction == 'left':
-                        self.Move_Idle.clip_draw((126*11) - ((self.move_frame+1) * 126),  0, 126, 150, self.x, self.y)
-                elif self.act_state == 'sitdown':
-                    if self.direction == 'right':
-                        self.Act_SitDown.clip_draw(self.act_frame * 186,  150, 186, 150, self.x, self.y)
-                    elif self.direction == 'left':
-                        self.Act_SitDown.clip_draw((186*10) - ((self.act_frame+1) * 186),  0, 186, 150, self.x, self.y)
-                elif self.act_state == 'lookup':
-                    if self.direction == 'right':
-                        self.Act_IdleLookUp.clip_draw(self.act_frame * 123,  150, 123, 150, self.x, self.y)
-                    elif self.direction == 'left':
-                        self.Act_IdleLookUp.clip_draw((123*11) - ((self.act_frame+1) * 123),  0, 123, 150, self.x, self.y)
-        elif self.move_state == 'run':
-            if self.act_state == 'ready':
-                if self.direction == 'right':
-                    self.Move_Run.clip_draw(self.move_frame * 110,  150, 110, 150, self.x, self.y)
+        if self.state == STAND:
+            if self.direction == RIGHT:
+                self.image.clip_draw(56 * self.frame, 25*self.state, 56, 25, self.x, self.y, 80,50)
+            elif self.direction == LEFT:
+                self.image.clip_draw(560-(56 * (self.frame+1)), 25*self.state, 56, 25, self.x-25, self.y, 80,50)
+        elif self.state == RUN:
+            if self.direction == RIGHT:
+                self.image.clip_draw(56 * self.frame, 25*self.state, 56, 25, self.x+3, self.y, 80,70)
+            elif self.direction == LEFT:
+                self.image.clip_draw(560-(56 * (self.frame+1)), 25*self.state, 56, 25, self.x-25-3, self.y, 80,70)
+        elif self.state == JUMP:
+            if self.direction == RIGHT:
+                self.image.clip_draw(56 * self.frame, 25*(self.state-1), 56, 25, self.x, self.y, 80,50)
+            elif self.direction == LEFT:
+                self.image.clip_draw(560-(56 * (self.frame+1)), 25*(self.state-1), 56, 25, self.x-25, self.y, 80,50)
 
-                elif self.direction == 'left':
-                    self.Move_Run.clip_draw(self.move_frame * 110,  0, 110, 150, self.x, self.y)
-
-def enter():
-
-    global marco,grass
-
-    marco = Marco()
-    grass = Map_1()
-
-def exit():
-
-    global boy,grass
-    del(boy)
-    del(grass)
-
-def pause():
-    pass
-
-def resume():
-    pass
-
-def handle_events():
-
-    global running
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            game_framework.quit()
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-                game_framework.change_state(title_state)
-                running = False
-        else:
-            marco.handle_event(event)
-
-def update():
-
-    marco.update()
-
-def draw():
-    clear_canvas()
-    delay(0.05)
-    grass.draw()
-    marco.draw()
-    update_canvas()
+delay(0.05)
 
 
